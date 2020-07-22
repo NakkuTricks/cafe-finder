5d8ed14d84cd6ccff6d01c88b732b7f5f68af33e
Maxim <nakkutricks@gmail.com>
2020-07-22 15:06:05 +0700

    changed the readme, connected the elementui and checked the application.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import { Button, Dialog } from "element-ui";
+
+Vue.use(Button);
+Vue.use(Dialog);
 
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
   render: h => h(App)
 }).$mount("#app");
